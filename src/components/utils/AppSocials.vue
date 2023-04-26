8ff166d30c87cfb7031f44d19f5224e0e2c0825d
SheikhUsamaDev <82177388+SheikhUsamaDev@users.noreply.github.com>
2023-04-26 23:10:25 +0500

    initial commit

---
+++ src/components/utils/AppSocials.vue
@@ -0,0 +1,25 @@
+<template>
+	<ul class="grid grid-flow-col gap-4 text-3xl mt-2 p-1">
+		<li class="text-gray-800 hover:text-blue-700">
+			<a href="https://www.facebook.com/profile.php?id=100004709029243"
+				><i class="fab fa-facebook"></i
+			></a>
+		</li>
+		<li class="text-gray-800 hover:text-black">
+			<a href="https://github.com/SheikhUsamaDev"><i class="fab fa-github"></i></a>
+		</li>
+
+		<li class="text-gray-800 hover:text-blue-500">
+			<a href="https://www.linkedin.com/in/usama-sheikh-35a414182/"
+				><i class="fab fa-linkedin"></i
+			></a>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
